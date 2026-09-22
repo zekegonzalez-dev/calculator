d4645ab558c0841ce3f2974bb33b46bf9799074e
Ezequiel Gonzalez <124253762+zekegonzalez-dev@users.noreply.github.com>
2026-09-22 22:11:43 +0800

    updated js to allow reinput after operation
    Allows user to reinput after an operation like 12 +12 =

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let operandOne;
 let operandTwo;
 let operator;
+let isEvaluated = false;
+
 
 //Operations
 function add(a,b){
@@ -63,41 +65,47 @@ const button = document.querySelectorAll('button');
 const display = document.querySelector('.display');
 
 button.forEach((button)=> button.addEventListener('click', (event)=>{
-
+//Operand One = null Operator = Null Operand Two - for entering the first digit
 if(operandOne==null && operandTwo==null && !isOperator(button.value) && isValidNumber(button.value)){
    operandOne = button.value;
    display.textContent=operandOne;
    console.log("operandOne: ",operandOne)
 }
+//Operand One = not null Operator = Null Operand Two = null - for entering more digits for first operand
 else if(operandOne!=null && operandTwo==null && !isOperator(button.value) && operator==null && isValidNumber(button.value)){
     operandOne+=button.value;
      display.textContent=operandOne;
      console.log("operandOne: ",operandOne)
 }
+ //for setting the operator
 else if(operandOne!=null && operandTwo==null && isOperator(button.value)){
     if(button.value!=="=" && button.value!=='clear'){
     operator=button.value
     console.log(operator);
     }
 }
-
+// setting the second operand first digit
 else if(operandOne!=null && operator!=null && operandTwo==null && !isOperator(button.value) && isValidNumber(button.value)){
      operandTwo = button.value;
      display.textContent=operandTwo;
       console.log("operandTwo: ", operandTwo)
-}
-else if(operandOne!=null && operandTwo!=null && operator!=null && !isOperator(button.value) && isValidNumber(button.value)){
+}// setting the second operand but adding more digits
+else if(operandOne!=null && operandTwo!=null && operator!=null && !isOperator(button.value) && isValidNumber(button.value) && isEvaluated===false){
      operandTwo+=button.value;
      display.textContent=operandTwo;
      console.log("operandTwo: ", operandTwo)
 }
+// calling the operate function
 else if(operandOne!=null && operandTwo!=null && (button.value==="=" || isOperator(button.value)) ){
     let result = operate(operandOne,operandTwo,operator);
     display.textContent= result;
     operandOne = result;
     if(button.value!=="=")operator = button.value;
-    console.log(operandOne);
-      console.log("operator: ",operator);
+    isEvaluated = true;
+    console.log("operandOne:",operandOne);
+    console.log("operator: ",operator);
+    console.log("operandTwo:",operandTwo);
+    //clear
 }else if(button.value==="clear"){
     operandOne=null;
     operandTwo=null;
@@ -106,6 +114,14 @@ else if(operandOne!=null && operandTwo!=null && (button.value==="=" || isOperato
     console.log("operandTwo: ",operandTwo);
     console.log("operator: ",operator);
     display.textContent=null;
+    //
+}else if(isEvaluated===true && isValidNumber(button.value)){
+    operandOne = button.value;
+    operandTwo = null;
+    operator = null;
+    isEvaluated = false;
+   display.textContent=operandOne;
+   console.log("operandOne: ",operandOne)
 }
 }));
 
