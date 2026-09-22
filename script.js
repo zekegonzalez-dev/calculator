681fd9ffc082cbc589cd9083c077e0919218458e
zekegonzalez-dev <ezequielg070901@gmail.com>
2026-09-22 15:03:59 +0800

    Added Input, Variable Assignment, Linear Calculation

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function divide(a,b){
 function operate(operandOne,operandTwo,operator){
     switch(operator){
         case '+':
-            return add(operandOne,operandTwo);  
+            return add(parseInt(operandOne),parseInt(operandTwo));  
         case '-':
             return subtract(operandOne,operandTwo);
         case '*':
@@ -33,3 +33,57 @@ function operate(operandOne,operandTwo,operator){
         default:break;
     }
 }
+
+function isOperator(inputOperator){
+    switch(inputOperator){
+        case '+':
+            return true;
+        case '-':
+            return true;
+        case '*':
+           return true;
+        case '/':
+             return true;
+        case '=':
+            return true;
+        default:
+            return false;
+    }
+}
+
+const button = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+button.forEach((button)=> button.addEventListener('click', (event)=>{
+if(operandOne==null && operandTwo==null && !isOperator(button.value)){
+   operandOne = button.value;
+   display.textContent=operandOne;
+   console.log("operandOne: ",operandOne)
+}
+else if(operandOne!=null && operandTwo==null && !isOperator(button.value) && operator==null){
+    operandOne+=button.value;
+     display.textContent=operandOne;
+     console.log("operandOne: ",operandOne)
+}
+else if(operandOne!=null && operandTwo==null && isOperator(button.value) ){
+    operator=button.value
+    console.log(operator);
+}
+else if(operandOne!=null && operator!=null && operandTwo==null && !isOperator(button.value)){
+     operandTwo = button.value;
+     display.textContent=operandTwo;
+      console.log("operandTwo: ", operandTwo)
+}
+else if(operandTwo!=null && operator!=null && !isOperator(button.value)){
+     operandTwo+=button.value;
+     display.textContent=operandTwo;
+     console.log("operandTwo: ", operandTwo)
+} 
+else if(button.value==="="){
+    let result = operate(operandOne,operandTwo,operator);
+    display.textContent= result;
+    operandOne = result;
+}
+}));
+
+
