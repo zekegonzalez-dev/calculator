4677eed0d7da3bd6431f3ddf91eacebc5cadb17f
Ezequiel Gonzalez <124253762+zekegonzalez-dev@users.noreply.github.com>
2026-09-22 17:54:15 +0800

    Added clear function and continuous operation
    Clear functionality works,
    
    Need to resolve issue where after an operation, it appends the value to the second operator rather than starting a new operation.

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ function operate(operandOne,operandTwo,operator){
         case '+':
             return add(parseInt(operandOne),parseInt(operandTwo));  
         case '-':
-            return subtract(operandOne,operandTwo);
+            return subtract(parseInt(operandOne),parseInt(operandTwo));
         case '*':
-           return multiply(operandOne,operandTwo);
+           return multiply(parseInt(operandOne),parseInt(operandTwo));
         case '/':
-           return divide(operandOne,operandTwo);
+           return divide(parseInt(operandOne),parseInt(operandTwo));
         default:break;
     }
 }
@@ -44,45 +44,68 @@ function isOperator(inputOperator){
            return true;
         case '/':
              return true;
-        case '=':
-            return true;
         default:
             return false;
     }
+}
+
+function isValidNumber(inputNumber){
+    const validNumbers =["0","1","2","3","4","5","6","7","8","9"];
+    if(validNumbers.includes(inputNumber)){
+        return true;
+    }else{
+        return false;
+    }
+
 }
 
 const button = document.querySelectorAll('button');
 const display = document.querySelector('.display');
 
 button.forEach((button)=> button.addEventListener('click', (event)=>{
-if(operandOne==null && operandTwo==null && !isOperator(button.value)){
+
+if(operandOne==null && operandTwo==null && !isOperator(button.value) && isValidNumber(button.value)){
    operandOne = button.value;
    display.textContent=operandOne;
    console.log("operandOne: ",operandOne)
 }
-else if(operandOne!=null && operandTwo==null && !isOperator(button.value) && operator==null){
+else if(operandOne!=null && operandTwo==null && !isOperator(button.value) && operator==null && isValidNumber(button.value)){
     operandOne+=button.value;
      display.textContent=operandOne;
      console.log("operandOne: ",operandOne)
 }
-else if(operandOne!=null && operandTwo==null && isOperator(button.value) ){
+else if(operandOne!=null && operandTwo==null && isOperator(button.value)){
+    if(button.value!=="=" && button.value!=='clear'){
     operator=button.value
     console.log(operator);
+    }
 }
-else if(operandOne!=null && operator!=null && operandTwo==null && !isOperator(button.value)){
+
+else if(operandOne!=null && operator!=null && operandTwo==null && !isOperator(button.value) && isValidNumber(button.value)){
      operandTwo = button.value;
      display.textContent=operandTwo;
       console.log("operandTwo: ", operandTwo)
 }
-else if(operandTwo!=null && operator!=null && !isOperator(button.value)){
+else if(operandOne!=null && operandTwo!=null && operator!=null && !isOperator(button.value) && isValidNumber(button.value)){
      operandTwo+=button.value;
      display.textContent=operandTwo;
      console.log("operandTwo: ", operandTwo)
-} 
-else if(button.value==="="){
+}
+else if(operandOne!=null && operandTwo!=null && (button.value==="=" || isOperator(button.value)) ){
     let result = operate(operandOne,operandTwo,operator);
     display.textContent= result;
     operandOne = result;
+    if(button.value!=="=")operator = button.value;
+    console.log(operandOne);
+      console.log("operator: ",operator);
+}else if(button.value==="clear"){
+    operandOne=null;
+    operandTwo=null;
+    operator=null;
+    console.log("operandOne: ",operandOne);
+    console.log("operandTwo: ",operandTwo);
+    console.log("operator: ",operator);
+    display.textContent=null;
 }
 }));
 
